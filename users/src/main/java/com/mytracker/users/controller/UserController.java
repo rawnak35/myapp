919d22bd4e8abc45bdeeea45db42702d562f8c00
rawnaks <rawnaks@xsense.co>
2026-01-01 10:54:30 +0200

    add springdoc openai

--- users/src/main/java/com/mytracker/users/controller/UserController.java
+++ users/src/main/java/com/mytracker/users/controller/UserController.java
@@ -6,14 +6,13 @@ import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 // "services" - api requests
-@Controller
+@RestController
 @RequestMapping("/api/users")
 @AllArgsConstructor
 @Validated
